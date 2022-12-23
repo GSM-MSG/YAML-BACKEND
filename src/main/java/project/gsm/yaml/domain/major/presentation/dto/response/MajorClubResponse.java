160e86c29a2305c78c73d974ac8bcfe35c3adbec
김태오 <taeo0328@gmail.com>
2022-12-23 18:12:05 +0900

    전공동아리 Response 작성

---
+++ src/main/java/project/gsm/yaml/domain/major/presentation/dto/response/MajorClubResponse.java
@@ -0,0 +1,11 @@
+package project.gsm.yaml.domain.major.presentation.dto.response;
+
+import lombok.Builder;
+
+@Builder
+public class MajorClubResponse {
+
+    private String name;
+
+    private Integer total;
+}
