553dd6f18530c769c11c358f1b27f866ee355700
김태오 <taeo0328@gmail.com>
2022-12-23 15:10:19 +0900

    Service 어노테이션 깜빡...

--- src/main/java/project/gsm/yaml/domain/major/presentation/service/impl/ContestServiceImpl.java
+++ src/main/java/project/gsm/yaml/domain/major/presentation/service/impl/ContestServiceImpl.java
@@ -1,6 +1,7 @@
 package project.gsm.yaml.domain.major.presentation.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import project.gsm.yaml.domain.major.entity.Major;
 import project.gsm.yaml.domain.major.presentation.dto.response.ContestResponse;
 import project.gsm.yaml.domain.major.presentation.dto.response.InsideContestResponse;
@@ -12,6 +13,7 @@ import project.gsm.yaml.domain.user.utils.UserUtil;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 @RequiredArgsConstructor
 public class ContestServiceImpl implements ContestService {
 
--- src/main/java/project/gsm/yaml/domain/major/presentation/service/impl/PrizeServiceImpl.java
+++ src/main/java/project/gsm/yaml/domain/major/presentation/service/impl/PrizeServiceImpl.java
@@ -1,6 +1,7 @@
 package project.gsm.yaml.domain.major.presentation.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import project.gsm.yaml.domain.major.entity.Major;
 import project.gsm.yaml.domain.major.presentation.dto.response.InsidePrizeResponse;
 import project.gsm.yaml.domain.major.presentation.dto.response.PrizeResponse;
@@ -12,6 +13,7 @@ import project.gsm.yaml.domain.user.utils.UserUtil;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 @RequiredArgsConstructor
 public class PrizeServiceImpl implements PrizeService {
 
