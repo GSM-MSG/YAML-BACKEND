7d811a16bd4c00f81a0384ccc6fc268da1733b2c
hajeu <s22072@gsm.hs.kr>
2022-12-25 01:32:05 +0900

    repository modify

--- src/main/java/project/gsm/yaml/domain/humanities/repository/BookRepository.java
+++ src/main/java/project/gsm/yaml/domain/humanities/repository/BookRepository.java
@@ -3,5 +3,8 @@ package project.gsm.yaml.domain.humanities.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.gsm.yaml.domain.humanities.entity.Book;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findById(Long id);
 }
--- src/main/java/project/gsm/yaml/domain/humanities/repository/VolunteerRepository.java
+++ src/main/java/project/gsm/yaml/domain/humanities/repository/VolunteerRepository.java
@@ -3,5 +3,8 @@ package project.gsm.yaml.domain.humanities.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.gsm.yaml.domain.humanities.entity.Volunteer;
 
+import java.util.Optional;
+
 public interface VolunteerRepository extends JpaRepository<Volunteer, Long> {
+    Optional<Volunteer> findById(Long id);
 }
