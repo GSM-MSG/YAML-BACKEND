02798ae6261078f16809fc88cbb67a49f6e0cc4d
김태오 <taeo0328@gmail.com>
2022-12-24 13:47:44 +0900

    위부 대회 참가 Dto작성

---
+++ src/main/java/project/gsm/yaml/domain/major/presentation/dto/request/OutsideContestRequest.java
@@ -0,0 +1,11 @@
+package project.gsm.yaml.domain.major.presentation.dto.request;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class OutsideContestRequest {
+
+    private final String name;
+
+    private final String fileURL;
+}
