c9cfb882097f2a323471720c932f56403d1b11c4
hajeu <s22072@gsm.hs.kr>
2022-12-24 21:14:29 +0900

    modify code

--- src/main/java/project/gsm/yaml/domain/humanities/service/impl/VolunteerServiceImpl.java
+++ src/main/java/project/gsm/yaml/domain/humanities/service/impl/VolunteerServiceImpl.java
@@ -2,14 +2,12 @@ package project.gsm.yaml.domain.humanities.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import project.gsm.yaml.domain.humanities.presentation.dto.response.AwardsResponse;
-import project.gsm.yaml.domain.humanities.presentation.dto.response.PrizeResponse;
+import project.gsm.yaml.domain.humanities.entity.Humanities;
 import project.gsm.yaml.domain.humanities.presentation.dto.response.VolunteerResponse;
 import project.gsm.yaml.domain.humanities.presentation.dto.response.VolunteersResponse;
 import project.gsm.yaml.domain.humanities.repository.VolunteerRepository;
 import project.gsm.yaml.domain.humanities.service.VolunteerService;
 import project.gsm.yaml.domain.humanities.utils.HumanitiesCaculateTotalUtil;
-import project.gsm.yaml.domain.user.entity.User;
 import project.gsm.yaml.domain.user.utils.UserUtil;
 
 import java.util.List;
@@ -21,16 +19,15 @@ public class VolunteerServiceImpl implements VolunteerService {
 
     private final UserUtil userUtil;
     private final HumanitiesCaculateTotalUtil caculateTotalUtil;
-
     private final VolunteerRepository volunteerRepository;
 
     @Override
     public VolunteersResponse volunteersExecute() {
-        User user = userUtil.currentUser();
-        List<VolunteerResponse> volunteerResponseList = user.getHumanities().getVolunteers().stream()
+        Humanities user = userUtil.currentUser().getHumanities();
+        List<VolunteerResponse> volunteerResponseList = user.getVolunteers().stream()
                 .map(VolunteerResponse::new)
                 .collect(Collectors.toList());
-        int hour = user.getHumanities().getVolunteers().stream().map(volunteer -> volunteer.getHour()).reduce(0, (a, b) -> a+b);
+        int hour = user.getVolunteers().stream().map(volunteer -> volunteer.getHour()).reduce(0, (a, b) -> a+b);
         int total = caculateTotalUtil.calculateVounteers(hour);
         return VolunteersResponse.builder()
                 .list(volunteerResponseList)
