a2070a50e8005282c32b86744a89853e49577d2b
김태오 <taeo0328@gmail.com>
2022-12-24 13:58:03 +0900

    repository 작성과 requestDto 작성

--- src/main/java/project/gsm/yaml/domain/major/presentation/dto/request/OutsideContestRequest.java
+++ src/main/java/project/gsm/yaml/domain/major/presentation/dto/request/OutsideContestRequest.java
@@ -1,7 +1,10 @@
 package project.gsm.yaml.domain.major.presentation.dto.request;
 
+import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public class OutsideContestRequest {
 
