0c1e9bf6f39474db99d09015917b2480516183ad
hajeu <s22072@gsm.hs.kr>
2022-12-24 20:53:32 +0900

    import 제거

--- src/main/java/project/gsm/yaml/domain/humanities/repository/AwardsRepository.java
+++ src/main/java/project/gsm/yaml/domain/humanities/repository/AwardsRepository.java
@@ -1,10 +1,7 @@
 package project.gsm.yaml.domain.humanities.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import project.gsm.yaml.domain.humanities.entity.Awards;
-
-import java.util.Optional;
 
 public interface AwardsRepository extends JpaRepository<Awards, Long> {
 
--- src/main/java/project/gsm/yaml/domain/humanities/repository/HumanitiesRepository.java
+++ src/main/java/project/gsm/yaml/domain/humanities/repository/HumanitiesRepository.java
@@ -1,7 +1,6 @@
 package project.gsm.yaml.domain.humanities.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import project.gsm.yaml.domain.humanities.entity.Humanities;
 
 public interface HumanitiesRepository extends JpaRepository<Humanities, Long> {
